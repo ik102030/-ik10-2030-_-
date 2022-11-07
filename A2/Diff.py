e8aa871bfd9c3f3782763dbfe4c76a9cf0045d5d
ik102030 <117352198+ik102030@users.noreply.github.com>
2022-11-07 13:59:38 +0000

    Update Diff.py

--- A2/Diff.py
+++ A2/Diff.py
@@ -1,7 +1,8 @@
+#import Diff
 from difflib import Differ
- 
-with open('file1.txt') as file_1, open('file2.txt') as file_2:
+#Open file 
+with open('backup.py') as backup_1, open('backup2.py') as backup_2:
     differ = Differ()
  
-    for line in differ.compare(file_1.readlines(), file_2.readlines()):
+    for line in differ.compare(backup_1.readlines(), backup_2.readlines()):
         print(line) 
