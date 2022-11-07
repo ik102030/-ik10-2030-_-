ab039a6031cdc599a6d19d1506943d33eea5407e
ik102030 <117352198+ik102030@users.noreply.github.com>
2022-11-07 14:15:50 +0000

    Create backup2.py

---
+++ A2/backup2.py
@@ -0,0 +1,36 @@
+#importing libraries 
+import getpass
+import telnetlib
+def backup():
+    user = str(input('Enter your telnet username: '))
+    password = getpass.getpass()
+
+    #opening myswitches and creating it in the python script
+    f = open('myswitches', 'w')
+    f.write("192.168.232.131")
+    f.close()
+    f=open('myswitches', 'r')
+
+    for ip in f:
+        ip=ip.strip()
+        print ('Get running config from switch ' + (ip))
+        HOST = ip 
+        tn = telnetlib.Telnet(HOST)
+        tn.read_until(b'Username: ')
+        tn.write(user.encode('ascii') + b'\n')
+        if password:
+            tn.read_until(b'Password: ') 
+            tn.write(password.encode('ascii') + b'\n')
+        tn.write(b"enable\n")  
+        tn.read_until(b'Password: ') 
+        tn.write(("class123!").encode('ascii') + b'\n')
+        tn.write(b"terminal length 0\n")
+        tn.write(b"show run\n")
+        tn.write(b'exit\n')
+
+        readoutput = tn.read_all()
+        saveoutput = open("switch" + HOST, "w")
+        saveoutput.write(readoutput.decode('ascii'))
+        saveoutput.write("\n")
+        saveoutput.close
+backup()   
