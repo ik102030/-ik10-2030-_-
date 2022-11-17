00ba2aea57e086b023cca8c45f986c31b1734d30
ik102030 <117352198+ik102030@users.noreply.github.com>
2022-11-17 11:00:19 +0000

    Update Diff.py

--- A2/Diff.py
+++ A2/Diff.py
@@ -2,12 +2,12 @@
 from difflib import Differ
 import os
 #Open files 
-with open('backup.py') as backup_1, open('startup-backuprunningconfig.py') as startup-backuprunningconfig._1:
+with open('backup2.txt') as backup_2:
     differ = Differ()
-with open('backup.py') as backup_1, open('backup3.py') as backup_3:
+with open('backup3.txt') as backup_3:
     differ = Differ()
 #Read contents and call the compare function with the use of differ class object 
-    for line in differ.compare(backup_1.readlines(), backup_3.readlines()):
+    for line in differ.compare(backup_2.readlines(), backup_3.text()):
         print(line)
 
 compare = os.system("diff myswitches myswitches2 myswitches3")
@@ -15,11 +15,11 @@ print(compare)
 
 
         #Open files 
-with open('backup.py') as backup_1, open('backup2.py') as backup_2:
+with open('backup.py') as backup_2, open('backup2.py') as backup_2:
     differ = Differ()
-with open('backup.py') as backup_1, open('backup2.py') as backup_2:
+with open('backup.py') as backup_3, open('backup3.py') as backup_2:
     differ = Differ()
 
 #Read contents and call the compare function with the use of differ class object 
-    for line in differ.compare(backup_1.readlines(), backup_2.readlines()):
+    for line in differ.compare(backup_2.readlines(), backup_3.readlines()):
         print(line)
