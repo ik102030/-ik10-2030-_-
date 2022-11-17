3a6c165137da2f6122f4fdf10fe050b6e2fdc50a
ik102030 <117352198+ik102030@users.noreply.github.com>
2022-11-17 10:59:50 +0000

    Update backup3.py

--- A2/backup3.py
+++ A2/backup3.py
@@ -7,7 +7,7 @@ def backup():
 
     #opening myswitches and creating it in the python script
     f = open('myswitches3', 'w')
-    f.write("192.168.232.131")
+    f.write("192.168.56.101")
     f.close()
     f=open('myswitches3', 'r')
 
@@ -33,4 +33,4 @@ def backup():
         saveoutput.write(readoutput.decode('ascii'))
         saveoutput.write("\n")
         saveoutput.close
-backup()    
+backup()  
