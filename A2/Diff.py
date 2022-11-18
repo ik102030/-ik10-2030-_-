2aa596a4cdd864dc073ae4e7bba84f0685e44952
ik102030 <117352198+ik102030@users.noreply.github.com>
2022-11-18 21:01:56 +0000

    Update Diff.py

--- A2/Diff.py
+++ A2/Diff.py
@@ -1,5 +1,6 @@
 #import libraries
 from difflib import Differ
+import os
 #Open files 
 with open('backup2.txt') as backup_2:
     differ = Differ()
@@ -9,11 +10,14 @@ with open('backup3.txt') as backup_3:
     for line in differ.compare('backup_2.readlines()','backup_3.readlines()'):
         print(line)
 
+#compare function
+compare = os.system("diff myswitches myswitches2 myswitches3")
+print(compare)
 
         #Open files 
-with open('backup.py') as backup_2, open('backup2.py') as backup_2:
+with open('backup2.txt') as backup_2, open('backup2.txt') as backup_2:
     differ = Differ()
-with open('backup.py') as backup_3, open('backup3.py') as backup_2:
+with open('backup3.txt') as backup_3, open('backup3.txt') as backup_2:
     differ = Differ()
 
 #Read contents and call the compare function with the use of differ class object 
